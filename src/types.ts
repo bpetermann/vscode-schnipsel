7e34cb14c9c6bfbcb10de20239beeada78ce908c
bpetermann <benjamin.petermann@gmx.at>
2025-08-01 22:32:28 +0200

    chore: add class keyword type

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ export type Tokens = Array<string>;
 
 export type TokenHandler = [test: () => boolean, handler: () => void];
 
-export const keywords = ['function', 'type', 'interface'] as const;
+export const keywords = ['function', 'type', 'interface', 'class'] as const;
 
 export type KeywordType = (typeof keywords)[number];
 
