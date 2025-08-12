c8b08ae311a485e2cd97ab45a3e24f74a3662ca4
bpetermann <benjamin.petermann@gmx.at>
2025-08-12 20:56:30 +0200

    chore: cleanup extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,14 @@
-import * as vscode from 'vscode';
+import { ExtensionContext } from 'vscode';
 import { Parser } from './Parser';
 import { Service } from './Service';
 import { Snippet } from './Snippet';
 import { ParserFactory, SnippetFactory } from './types';
 
-export function activate(context: vscode.ExtensionContext) {
-  const parseFactory: ParserFactory = (text: string): Parser =>
-    new Parser(text);
-  const snippetFactory: SnippetFactory = (body, language, name) =>
-    new Snippet(body, language, name);
+const parseFactory: ParserFactory = (...args): Parser => new Parser(...args);
+const snippetFactory: SnippetFactory = (...args) => new Snippet(...args);
 
-  const service = new Service(context, parseFactory, snippetFactory);
-  service.registerCommands();
+export function activate(context: ExtensionContext) {
+  new Service(context, parseFactory, snippetFactory).registerCommands();
 }
 
 export function deactivate() {}
